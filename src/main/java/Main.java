f5ad4073cb97e8e84e8efde47065418bdf516c21
Javier Gomez <Jgomez@cambridgeassociates.com>
2017-04-21 16:47:22 -0600

    4.3 - companion object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,29 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PersonaJava persona = new PersonaJava("Javier");
-        final PersonaJava persona2 = new PersonaJava(persona); // copy
-        final PersonaJava persona3 = new PersonaJava(persona); // copy
-        persona3.setLastName("Gomez");
+        int count = PersonKT.staticMethodCountLetters("Javier");
 
-        if(persona.equals(persona2))
-            System.out.println("Hello "+persona.getFirstName());
-        else
-            System.out.println("not equal");
-
-        if(!persona.equals(persona3))
-            System.out.println("old "+persona.getLastName()+" new "+persona3.getLastName());
-
-        for(int i=0;i < 3 ;i++){
-            StringUtil.coffeNow("zz "+i+" zz");
-        }
-
-        // No equivalent to infix function
-
-        PersonKT personKT = new PersonKT("Pepe","Perez",20);
-        System.out.println("Kotlin: "+personKT.toString());
-
-        personKT.specialConstructor("Pedro");// method Overload
+        System.out.println("count: "+count);
 
     }
 
--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -2,40 +2,20 @@ import util.coffeNow
 
 fun main(args: Array<String>) {
 
-    val persona:PersonKT = PersonKT(firstName = "Javier")
-    val persona2 = persona.copy()
-    val persona3 = persona.copy(lastName = "Gomez")
+    val count = PersonKT.staticMethodCountLetters("Javier")
 
-    val message = if(persona == persona2) "Hello ${persona.firstName}"  else "not equal"
-
-    println(message)
-
-    if (persona3 != persona)
-        println("old ${persona.lastName} New ${persona3.lastName}")
-
-    for(i in 1..3){
-        println("zz $i zz".coffeNow()) //Extension Functions
-    }
-
-    println("Hola " add "Mundo!") //infix function
-
-    val personaJ = PersonaJava("Pepe","Perez",20) // Java in Kotlin
-    println("Java: ${personaJ.toString()}")
+    println("count: $count")
 
 }
 
-infix fun String.add(str:String):String{
-    return this + str
-}
 
 data class PersonKT(var firstName:String="", var lastName:String="", var age: Int=0){
 
-    @JvmOverloads// method Overload
-    fun specialConstructor(firstName:String="", lastName:String="", age: Int=0){
-        this.firstName = firstName
-        this.lastName = lastName
-        this.age = age
+    constructor(firstName:String="") : this(firstName,lastName="",age=0)
 
+    companion object{
+        @JvmStatic()
+        fun staticMethodCountLetters(str:String)=str.length
     }
 
 }
