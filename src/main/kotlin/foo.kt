fb43d643da2671e07cf49cf302f2cec573d87615
Javier <gomezrondon@gmail.com>
2017-04-22 11:58:31 -0600

    2.2 ext fun dsl

--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -3,51 +3,24 @@ import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
 
-    val list = listOf("Fowler", "Beck", "Evans")
-   //val list = listOf(1, 2, 3, 4)
-    //val list = ArrayList<String>()
+ val persona = PersonKT()
 
-    var primero = ""
-    list.notNullOrEmpty {
-        primero =firstOrNull().toString()
+    persona{
+        firstName = "Javier"
+        lastName = "Gomez"
     }
 
+    println("Persona: $persona")
 
-
-/*
-    println("primero = $primero ")
-
-   val counter = list.notNullOrEmpty2 {
-                   // stream().count().toInt()
-                   firstOrNull().toString()
-                }
-
-    println("counter = $counter ")
-
-    list {
-        stream().forEach( {x -> println(x)})
-    }
-*/
 }
 
 
-inline operator fun <E: Any, T: Collection<E>> T?.invoke(func: T.() -> Unit): Unit {
-    if (this != null && this.isNotEmpty()) {
-        with (this) { func() }
+inline operator fun < T: PersonKT> T?.invoke(func: T.() -> Unit): Unit {
+    if (this != null) {
+        with (this) {func()  }
     }
 }
 
-inline fun <E: Any, T: Collection<E>> T?.notNullOrEmpty(func: T.() -> Unit): Unit {
-    if (this != null && this.isNotEmpty()) {
-        with (this) { func() }
-    }
-}
-
-inline fun <E: Any, T: Collection<E>> T.notNullOrEmpty2(func: T.() -> Any): Any? {
-    if (this != null && this.isNotEmpty()) {
-        with (this) { return func()}
-    } else return null
-}
 
 data class PersonKT(var firstName:String?=null, var lastName:String?=null, var age: Int=0)
 
