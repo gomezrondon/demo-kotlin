eba1f6de5b461bdf25e4c47ebe901edb2cb31a58
Javier <gomezrondon@gmail.com>
2017-04-22 11:48:02 -0600

    2.1 inline functions with operator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PersonKT persona = new PersonKT();
-
-        System.out.println("name is: "+persona.getFirstName().length());//NullPointerException
-
     }
 
 }
--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -1,11 +1,53 @@
+import java.util.*
+import kotlin.collections.ArrayList
+
 fun main(args: Array<String>) {
 
-    val persona:PersonKT = PersonKT(firstName = null)
+    val list = listOf("Fowler", "Beck", "Evans")
+   //val list = listOf(1, 2, 3, 4)
+    //val list = ArrayList<String>()
 
-  //  println("name is: ${persona.firstName!!.length}") //KotlinNullPointerException
+    var primero = ""
+    list.notNullOrEmpty {
+        primero =firstOrNull().toString()
+    }
 
-    println("name is: ${persona.firstName?.length}")
 
+
+/*
+    println("primero = $primero ")
+
+   val counter = list.notNullOrEmpty2 {
+                   // stream().count().toInt()
+                   firstOrNull().toString()
+                }
+
+    println("counter = $counter ")
+
+    list {
+        stream().forEach( {x -> println(x)})
+    }
+*/
+}
+
+
+inline operator fun <E: Any, T: Collection<E>> T?.invoke(func: T.() -> Unit): Unit {
+    if (this != null && this.isNotEmpty()) {
+        with (this) { func() }
+    }
+}
+
+inline fun <E: Any, T: Collection<E>> T?.notNullOrEmpty(func: T.() -> Unit): Unit {
+    if (this != null && this.isNotEmpty()) {
+        with (this) { func() }
+    }
+}
+
+inline fun <E: Any, T: Collection<E>> T.notNullOrEmpty2(func: T.() -> Any): Any? {
+    if (this != null && this.isNotEmpty()) {
+        with (this) { return func()}
+    } else return null
 }
 
 data class PersonKT(var firstName:String?=null, var lastName:String?=null, var age: Int=0)
+
