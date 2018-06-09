ab5de49aad245eb3e87772d573d05ef16f2aa6ba
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-06-09 13:09:10 -0600

    fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,3 @@
-import util.StringUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -8,30 +7,6 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-
-        PersonaJava persona = new PersonaJava("Javier");
-        final PersonaJava persona2 = new PersonaJava(persona); // copy
-        final PersonaJava persona3 = new PersonaJava(persona); // copy
-        persona3.setLastName("Gomez");
-
-        if(persona.equals(persona2))
-            System.out.println("Hello "+persona.getFirstName());
-        else
-            System.out.println("not equal");
-
-        if(!persona.equals(persona3))
-            System.out.println("old "+persona.getLastName()+" new "+persona3.getLastName());
-
-        for(int i=0;i < 3 ;i++){
-            StringUtil.coffeNow("zz "+i+" zz");
-        }
-
-        // No equivalent to infix function
-
-        PersonKT personKT = new PersonKT("Pepe","Perez",20);
-        System.out.println("Kotlin: "+personKT.toString());
-
-        personKT.specialConstructor("Pedro");// method Overload
 
         //Lambda Expressions
         try {
