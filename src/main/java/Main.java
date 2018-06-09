f14171c02c7c2244291c71d1b4f94c3389447ff4
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-06-09 12:46:25 -0600

    fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,23 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        PersonaJava persona = new PersonaJava("Javier");
-        final PersonaJava persona2 = persona.copy(); // copy
-        final PersonaJava persona3 = persona.copy("Gomez"); // copy
-
-        if(persona.equals(persona2))
-            System.out.println("Hello "+persona.getFirstName());
-        else
-            System.out.println("not equal");
-
-        if(!persona.equals(persona3))
-            System.out.println("old "+persona.getLastName()+" new "+persona3.getLastName());
-
-        for(int i=0;i < 3 ;i++){
-            StringUtil.coffeNow("zz "+i+" zz");
-        }
-
-        // No equivalent to infix function
+        PersonKT personKT = new PersonKT("Pepe","Perez",20);
+        System.out.println("Kotlin: "+personKT.toString());
 
     }
 
--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -2,30 +2,11 @@ import util.coffeNow
 
 fun main(args: Array<String>) {
 
-    val persona:PersonKT = PersonKT(firstName = "Javier")
-    val persona2 = persona.copy()
-    val persona3 = persona.copy(lastName = "Gomez")
-
-    val message = if(persona == persona2) "Hello ${persona.firstName}"  else "not equal"
-
-    println(message)
-
-    if (persona3 != persona)
-        println("old ${persona.lastName} New ${persona3.lastName}")
-
-    for(i in 1..3){
-        println("zz $i zz".coffeNow()) //Extension Functions
-    }
-
-    println("Hola " add "Mundo!") //infix function
 
     val personaJ = PersonaJava("Pepe","Perez",20) // Java in Kotlin
     println("Java: ${personaJ.toString()}")
 
 }
 
-infix fun String.add(str:String):String{
-    return this + str
-}
 
 data class PersonKT(var firstName:String="", var lastName:String="", var age: Int=0)
