e98be4ad7a497ed31e425dc6d384d1aee1363e84
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-06-09 12:29:25 -0600

    fixing

--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -3,51 +3,55 @@ import kotlin.collections.ArrayList
 
 fun main(args: Array<String>) {
 
+    // test 1
     val list = listOf("Fowler", "Beck", "Evans")
-   //val list = listOf(1, 2, 3, 4)
+
+    var primero =""
+    list.notNullOrEmpty {
+        primero = firstOrNull().toString()
+    }
+
+    println("test 1 = $primero ")
+
+
+    //test 2 - The same but without a name
+    val list2 = listOf(1, 2, 3, 4)
     //val list = ArrayList<String>()
 
-    var primero = ""
-    list.notNullOrEmpty {
-        primero =firstOrNull().toString()
+     list2 {
+         primero = firstOrNull().toString()
     }
 
+    println("test 2 = $primero ")
 
 
-/*
-    println("primero = $primero ")
+    // test 3
+    val list3 = ArrayList<String>()
 
-   val counter = list.notNullOrEmpty2 {
-                   // stream().count().toInt()
-                   firstOrNull().toString()
-                }
-
-    println("counter = $counter ")
-
-    list {
-        stream().forEach( {x -> println(x)})
+    list3.notNullOrEmpty {
+        primero = firstOrNull().toString()
     }
-*/
+
+    println("test 3 = $primero ")
+
 }
 
 
-inline operator fun <E: Any, T: Collection<E>> T?.invoke(func: T.() -> Unit): Unit {
-    if (this != null && this.isNotEmpty()) {
-        with (this) { func() }
-    }
-}
 
+// Extends the functionality of the Collection class
 inline fun <E: Any, T: Collection<E>> T?.notNullOrEmpty(func: T.() -> Unit): Unit {
     if (this != null && this.isNotEmpty()) {
         with (this) { func() }
     }
 }
 
-inline fun <E: Any, T: Collection<E>> T.notNullOrEmpty2(func: T.() -> Any): Any? {
+
+// The same but without a name
+inline operator fun <E: Any, T: Collection<E>> T?.invoke(func: T.() -> Unit): Unit {
     if (this != null && this.isNotEmpty()) {
-        with (this) { return func()}
-    } else return null
+        with (this) { func() }
+    }
 }
 
-data class PersonKT(var firstName:String?=null, var lastName:String?=null, var age: Int=0)
+
 
