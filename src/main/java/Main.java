f008b20e81a491831a771cb242f22c16765d1c64
DESKTOP-8NG6QPL\gomez <gomezrondon@gmail.com>
2018-06-09 12:42:26 -0600

    fixing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,7 @@
-import util.StringUtil;
 
 public class Main {
 
     public static void main(String[] args) {
-
-        PersonaJava persona = new PersonaJava("Javier");
-        final PersonaJava persona2 = persona.copy(); // copy
-        final PersonaJava persona3 = persona.copy("Gomez"); // copy
-        persona3.setLastName("Gomez");
-
-        if(persona.equals(persona2))
-            System.out.println("Hello "+persona.getFirstName());
-        else
-            System.out.println("not equal");
-
-        if(!persona.equals(persona3))
-            System.out.println("old "+persona.getLastName()+" new "+persona3.getLastName());
-
-        for(int i=0;i < 3 ;i++){
-            StringUtil.coffeNow("zz "+i+" zz");
-        }
 
         // No equivalent to infix function
 
@@ -28,75 +10,3 @@ public class Main {
 }
 
 
-
-class PersonaJava{
-
-    private String firstName;
-    private String lastName;
-    int age;
-
-    public PersonaJava(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public PersonaJava(String firstName, String lastName, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-    }
-
-    public PersonaJava copy(){
-        return new PersonaJava(firstName,lastName,age);
-
-    }
-
-    public PersonaJava copy(String lastName){
-        return new PersonaJava(firstName,lastName,age);
-
-    }
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        PersonaJava that = (PersonaJava) o;
-
-        if (age != that.age) return false;
-        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
-        return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = firstName != null ? firstName.hashCode() : 0;
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + age;
-        return result;
-    }
-}
--- src/main/kotlin/foo.kt
+++ src/main/kotlin/foo.kt
@@ -2,21 +2,6 @@ import util.coffeNow
 
 fun main(args: Array<String>) {
 
-    val persona:PersonKT = PersonKT(firstName = "Javier")
-    val persona2 = persona.copy()
-    val persona3 = persona.copy(lastName = "Gomez")
-
-    val message = if(persona == persona2) "Hello ${persona.firstName}"  else "not equal"
-
-    println(message)
-
-    if (persona3 != persona)
-        println("old ${persona.lastName} New ${persona3.lastName}")
-
-    for(i in 1..3){
-        println("zz $i zz".coffeNow()) //Extension Functions
-    }
-
     println("Hola " add "Mundo!") //infix function
     
 }
@@ -25,4 +10,3 @@ infix fun String.add(str:String):String{
     return this + str
 }
 
-data class PersonKT(var firstName:String="", var lastName:String="", var age: Int=0)
